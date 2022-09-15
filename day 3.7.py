96fdf9b50c3ed36bd6f303f87ee0de0af7d718b7
yuvi444 <112838629+yuvi444@users.noreply.github.com>
2022-09-15 09:20:39 +0530

    Add files via upload

---
+++ day 3.7.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+s=int(input("Enter length of list:"))
+b=[]
+c=[]
+for i in range(0,s):
+    a=int(input("Enter element:"))
+    b.append(a)
+perm=permutations(b)
+for i in list(perm):
+    if i not in c:
+        c.append(i)
+print("OUTPUT=",c)
+        
